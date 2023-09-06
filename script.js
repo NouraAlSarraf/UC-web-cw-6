3fc8ca16c80bcd3c92a8cbe5a0a09b3d57d51265
NouraAlSarraf <noura14022008@outlook.com>
2023-09-06 20:37:30 +0300

    cw6

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ let score=0;
 let scoreHTML= document.getElementsById('score');
 function increase(){
     score =score +1 ;
-    scoreHTML.innerHTML= (score);
-}
+    scoreHTML.innerHTML= score;
+};
 
+function welcome(){
+    let name = prompt('enter your name');
+    document.getElementById('tittle').innerText = `Welcome ${name} to COOOKIEE CLICKEEERRR!!!!`;
 
+};
+welcome();
